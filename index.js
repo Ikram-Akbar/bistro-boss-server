28f078e6f4ace498502e9b28d50ec84f08c774f5
Ikram Akbar <ikramakbar71.com>
2023-08-22 15:57:26 +0600

    Send JWT token to back end for verification

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ require("dotenv").config();
 //middleware :
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 // app.use(cors);
 app.use(express.json());
@@ -36,6 +53,8 @@ async function run() {
     const CartCollection = client.db("Bistro-Boss").collection("carts");
 
     //JWT : 
+    //POST api : 
+
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1hr" });
@@ -112,18 +131,23 @@ async function run() {
 
     // GET Method : 
 
-    app.get("/carts", async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
-        res.send([])
-      } else {
-        const query = { email: email };
-        const result = await CartCollection.find(query).toArray();
-        res.send(result);
+        res.send([]);
       }
 
-    })
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'Forbidden access' })
+      }
+
+      const query = { email: email };
+      const result = await CartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // POST API : 
     app.post("/carts", async (req, res) => {
       const item = req.body;
